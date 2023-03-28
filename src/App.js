689e6f65ff459f4cbdadbad6b076eb1b1015d7e1
Sethumadhavan K <sethuksethu@gmail.com>
2023-03-28 14:25:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+
+  useEffect(()=>{
+    fetch("http://localhost:9000/").then(res=>{
+      console.log(res)
+    })
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
